5d01ccbecab1aae53dba2087b774889a12c5e281
alexlerikos <alerikos@gmail.com>
2015-11-11 03:39:04 -0800

    now stores locations of all instances of individual kmers in the query string

--- perl_ItsABlast.pl
+++ perl_ItsABlast.pl
@@ -9,7 +9,7 @@ $/ = ""; #set to paragraph mode
 $input_query_file = "q_test_string.txt";
 
 # file to handle the input data used to build our associative arrays
-$input_data_file = "perlblastdata.txt";
+$input_data_file = "perlblastdatas.txt";
 
 open(QUERY, "$input_query_file");
 open(DATA_IN,"$input_data_file");
@@ -19,10 +19,6 @@ $query = <QUERY>;
 chomp $query;
 @queryArray = split //, $query; # save duplicate string for comparison use after query
 					 # is parsed into the kmer hash
-print "query: $query\n";
-print "queryArray:\n"; 
-print "@queryArray\n";
-# print "tempquery: $querytemp\n";
 
 
 #set up k-mer length
@@ -36,14 +32,14 @@ $i = 0;
 while (length($query) >= $k) {
   $query =~ m/(.{$k})/; 
   # print "$1, $i \n";
-   if (! defined $kmer{$1}) {     #defined is a function that returns true
-                                  # if a value has already been assigned to
-                                  # $kmer{$1}, otherwise it returns false. 
-                                  # the ! character is the negation, so
-                                  # if $kmer{$1} has no value, then it will
-                                  # be assigned the value of $i, the position
-                                  # where the k-mer is first found.
-    $kmer{$1} = $i;       
+   if (! defined $kmer{$1}) {   # here we tell Perl that the value of a kmer entry will
+                         		# be an array. This is done by enclosing $i with [ ].
+                         		# More correctly, $kmer{$1} is a reference to an array
+                         		# whose first value is the value of $i.
+    $kmer{$1} = [$i];       
+   }
+   else {
+   	push (@{$kmer{$1}}, $i);
    }
    $i++;
    $query = substr($query, 1, length($query) -1);
@@ -53,47 +49,53 @@ while (length($query) >= $k) {
 
 # read data input file line by line to build kmer array
 while($line = <DATA_IN>){
-	print "line: $line\n\n";
 	@dataBaseLineArray = split //, $line;
-	# print "@dataBaseLineArray\n";
 	$i = 0;
 	while (length($line) >= $k) {
 		$line =~ m/(.{$k})/; 
 	  	# print "$1, $i \n";
-	   	if (defined $kmer{$1}) {      
-	    	$L  = 0; #store length of query and database matching charatures
-	    	$j = 0;
+	   	if (defined $kmer{$1}) {
+	   		foreach $querylocation (@{$kmer{$1}}){
+	   			# print "query location: $querylocation\n";
+	   		      
+		    	$L  = 0; #store length of query and database matching charatures
+		    	$j = 0;
 
-	    	#check charactures to right of starting index of matching kmer for matches
-	    	while ($queryArray[$kmer{$1}+$j] eq $dataBaseLineArray[$i+$j]){
-	    		# print "$j\n";
-	    		$j++;
-	    		$L++;
-	    	}
-	    	$subEnd = $i+$j-1;
-	    	$j = 1;
+		    	#check charactures to right of starting index of matching kmer for matches
+		    	while ($queryArray[$querylocation+$j] eq $dataBaseLineArray[$i+$j]){
+		    		# print "$j\n";
+		    		$j++;
+		    		$L++;
+		    	}
+		    	$subEnd = $i+$j-1;
+		    	$j = 1;
 
-	    	#check charatures to the left of the matching kmer starting index for match
-	    	while ($queryArray[$kmer{$1}-$j] eq $dataBaseLineArray[$i-$j]){
-	    		# print "$j\n";
-	    		$j++;
-	    		$L++;
-	    	}
-	    	$subStart = $i-$j+1;
-	    	#create string of all matching chars 
-	    	$matchString = join('',@dataBaseLineArray[$subStart..$subEnd]);
-	    	# print("Matching string: $matchString\n");
+		    	#check charatures to the left of the matching kmer starting index for match
+		    	while ($queryArray[$querylocation-$j] eq $dataBaseLineArray[$i-$j]){
+		    		# print "$j\n";
+		    		$j++;
+		    		$L++;
+		    	}
+		    	$subStart = $i-$j+1;
+		    	#create string of all matching chars 
+		    	$matchString = join('',@dataBaseLineArray[$subStart..$subEnd]);
+		    	# print("Matching string: $matchString\n");
 
-	    	if (!defined $stringHash{$subStart}){
-	    		$stringHash{$subStart} = $matchString;
-	    		print("Matching string: $matchString\n");
-	    		if ($L > 10){
-	    			print "Good HSP has been found\n";
-	    		}
-	    		print "Match Length = $L\n";
-	    	}
+		    	if (!defined $stringHash{$subStart}){
+		    		$stringHash{$subStart} = $matchString;
+		    		print("Matching string: '$matchString' with length $L\n");
+		    		if ($L > 10){
+		    			print "Good HSP has been found\n";
+		    		}
+		    	}
+		    }
 	   	}
 		$i++;
 	 	$line = substr($line, 1, length($line) -1);
 	}
 }
+# Uncomment below to view all kmer locations in the query string
+# foreach $kmerkey (keys(%kmer)) {
+#  $occrs = join(', ' , @{$kmer{$kmerkey}});
+#  print "The occurrences of string $kmerkey are in positions $occrs \n";
+# }
